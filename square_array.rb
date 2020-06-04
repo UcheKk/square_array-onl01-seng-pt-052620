87a743bc056dc9cea6e3b6dcf959be062bef2f80
Uche Nweke Jr <unwekejr@gmail.com>
2020-06-04 23:17:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,8 @@
 def square_array(array)
   new_array = []
   array.each do |number| 
-    number ** 2
     new_number = number ** 2
-    new_array.shift << new_number
+    new_array << new_number
     
 end
 
